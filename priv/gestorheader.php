734215b3d0b98b0f84dcac955a5d0b01087ade66
Niemand008 <111530855+Niemand008@users.noreply.github.com>
2023-01-18 12:24:36 +0000

    Header do Admin e do Gestor

---
+++ priv/gestorheader.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="gestorheader.css">
+    <title>Gestor Header</title>
+</head>
+<body>
+    <header>
+        <a href="gestorindex.php" class="logo">Parcesul</a>
+        <a href="../publ/login.php" class="logout">Logout</a>
+    </header>
+    
+
+    <div class="conteudo">
+        <div class="faixa">
+        <p class="botao">
+        <a href="../priv/users_view.php" class="mensagens">Ver mensagens</a>
+        </p>    
+    </div>
+    </div>
+</body>
+</html>
